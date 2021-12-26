3e396406e19fdd46838b9739119efca03d9e796a
mufty <gubriansky@sernato.sk>
2021-12-26 19:57:34 +0100

    simple tcp client

--- app/src/main/java/com/mufty/phony/MainActivity.java
+++ app/src/main/java/com/mufty/phony/MainActivity.java
@@ -2,13 +2,40 @@ package com.mufty.phony;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import com.mufty.phony.net.TcpClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    TcpClient tcpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new ConnectTask().execute("");
     }
+
+    public class ConnectTask extends AsyncTask<String, String, TcpClient> {
+
+        @Override
+        protected TcpClient doInBackground(String... strings) {
+            //we create a TCPClient object
+            tcpClient = new TcpClient(new TcpClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+            tcpClient.run("192.168.1.10", 18561);
+
+            return null;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/mufty/phony/net/TcpClient.java
@@ -0,0 +1,99 @@
+package com.mufty.phony.net;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TcpClient {
+
+    public static final String TAG = TcpClient.class.getSimpleName();
+    private String serverIp;
+    private int serverPort;
+    private String mServerMessage;
+    private OnMessageReceived mMessageListener = null;
+    private boolean mRun = false;
+    private PrintWriter mBufferOut;
+    private BufferedReader mBufferIn;
+
+    public TcpClient(OnMessageReceived listener){
+        mMessageListener = listener;
+    }
+
+    public void sendMessage(final String message) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if(mBufferOut != null){
+                    Log.d(TAG, "Sending: " + message);
+                    mBufferOut.println(message);
+                    mBufferOut.flush();
+                }
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+    public void stopClient(){
+        mRun = false;
+
+        if(mBufferOut != null){
+            mBufferOut.flush();
+            mBufferOut.close();
+        }
+
+        mMessageListener = null;
+        mBufferIn = null;
+        mBufferOut = null;
+        mServerMessage = null;
+    }
+
+    public void run(String ip, int port){
+        if(ip != null)
+            serverIp = ip;
+        if(port != 0)
+            serverPort = port;
+
+        mRun = true;
+
+        try {
+            InetAddress serverAddr = InetAddress.getByName(serverIp);
+
+            Log.d("TCP Client", "C: Connecting...");
+
+            Socket socket = new Socket(serverAddr, serverPort);
+
+            try {
+                mBufferOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                mBufferIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                while(mRun){
+                    mServerMessage = mBufferIn.readLine();
+
+                    if(mServerMessage != null && mMessageListener != null){
+                        mMessageListener.messageReceived(mServerMessage);
+                    }
+                }
+
+                Log.d("RESPONSE FROM SERVER", "S: Received Message '" + mServerMessage + "'");
+            } catch (Exception e) {
+                Log.e("TCP", "S: Error", e);
+            } finally {
+                socket.close();
+            }
+        } catch (Exception e) {
+            Log.e("TCP", "C: Error", e);
+        }
+    }
+
+    public interface OnMessageReceived {
+        public void messageReceived(String message);
+    }
+}
