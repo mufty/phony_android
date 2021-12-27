f0aff5a966171a9acaefb2b0d67de07fe45fea95
mufty <gubriansky@sernato.sk>
2021-12-27 19:07:02 +0100

    prep for audio playback

--- app/src/main/java/com/mufty/phony/MainActivity.java
+++ app/src/main/java/com/mufty/phony/MainActivity.java
@@ -1,9 +1,12 @@
 package com.mufty.phony;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.media.AudioAttributes;
+import android.media.AudioFormat;
+import android.media.AudioTrack;
 import android.os.AsyncTask;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.mufty.phony.net.TcpClient;
 
@@ -17,6 +20,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         new ConnectTask().execute("");
+
+        init();
+    }
+
+    public void init() {
+        int i = 0;
+        int minBufferSized = 512;
+        int sampleRate = 48000;
+        byte[] music = null;
+
+        AudioTrack at = new AudioTrack.Builder()
+                .setAudioAttributes(new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA) // USAGE_VOICE_COMMUNICATION
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC) // CONTENT_TYPE_SPEECH
+                        .build())
+                .setAudioFormat(new AudioFormat.Builder()
+                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                        .setSampleRate(sampleRate)
+                        .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                        .build())
+                .setBufferSizeInBytes(minBufferSized)
+                .build();
+
+        /*try{
+            music = new byte[512];
+            at.play();
+
+            while((i = is.read(music)) != -1)
+                at.write(music, 0, i);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        at.stop();
+        at.release();*/
     }
 
     public class ConnectTask extends AsyncTask<String, String, TcpClient> {
